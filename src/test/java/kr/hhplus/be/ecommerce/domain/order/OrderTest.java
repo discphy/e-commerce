a6003c36683ba38bed2766d0247637a4a3503807
discphy <dinophy@nate.com>
2025-04-18 00:00:25 +0900

    [TEST] 주문 도메인 주문 상품 NULL 검증 코드 추가

--- src/test/java/kr/hhplus/be/ecommerce/domain/order/OrderTest.java
+++ src/test/java/kr/hhplus/be/ecommerce/domain/order/OrderTest.java
@@ -12,6 +12,15 @@ class OrderTest {
 
     @DisplayName("주문 상품이 없는 주문을 생성할 수 없다.")
     @Test
+    void createWithoutOrderProducts() {
+        // when & then
+        assertThatThrownBy(() -> Order.create(1L, null, 0.0, null))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("주문 상품이 없습니다.");
+    }
+
+    @DisplayName("주문 상품이 비어있는 주문을 생성할 수 없다.")
+    @Test
     void createEmptyOrderProducts() {
         // when & then
         assertThatThrownBy(() -> Order.create(1L, null, 0.0, List.of()))
