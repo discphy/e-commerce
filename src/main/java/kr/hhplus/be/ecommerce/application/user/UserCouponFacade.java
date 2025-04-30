5db6c6e2d093337b4b6ae73c10cbe7432d3df6da
discphy <dinophy@nate.com>
2025-04-30 16:38:51 +0900

    [FEAT] 선착순 쿠폰 분산 락 적용

--- src/main/java/kr/hhplus/be/ecommerce/application/user/UserCouponFacade.java
+++ src/main/java/kr/hhplus/be/ecommerce/application/user/UserCouponFacade.java
@@ -5,6 +5,9 @@ import kr.hhplus.be.ecommerce.domain.coupon.CouponService;
 import kr.hhplus.be.ecommerce.domain.user.UserCouponInfo;
 import kr.hhplus.be.ecommerce.domain.user.UserCouponService;
 import kr.hhplus.be.ecommerce.domain.user.UserService;
+import kr.hhplus.be.ecommerce.support.lock.DistributedLock;
+import kr.hhplus.be.ecommerce.support.lock.LockStrategy;
+import kr.hhplus.be.ecommerce.support.lock.LockType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,6 +23,7 @@ public class UserCouponFacade {
     private final UserCouponService userCouponService;
 
     @Transactional
+    @DistributedLock(type = LockType.COUPON, key = "#criteria.couponId", strategy = LockStrategy.SPIN_LOCK)
     public void publishUserCoupon(UserCouponCriteria.Publish criteria) {
         userService.getUser(criteria.getUserId());
 
