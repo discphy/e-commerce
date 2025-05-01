2b78f02555cd9aad02dbdb4f52b4e0aae6458e76
discphy <dinophy@nate.com>
2025-05-01 15:16:39 +0900

    [BUG] 테스트 깨지는 코드 수정 리팩토링

--- src/test/java/kr/hhplus/be/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/kr/hhplus/be/ecommerce/EcommerceApplicationTests.java
@@ -1,10 +1,11 @@
 package kr.hhplus.be.ecommerce;
 
+import kr.hhplus.be.ecommerce.support.ContainerTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class EcommerceApplicationTests extends ContainerTestSupport {
 
 	@Test
 	void contextLoads() {
