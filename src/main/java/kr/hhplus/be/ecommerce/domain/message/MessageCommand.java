ec08379d502f42a76a1f91a013d1ab7c1e7acace
discphy <dinophy@nate.com>
2025-05-22 22:42:56 +0900

    [FEAT] Message 이벤트 작성

--- src/main/java/kr/hhplus/be/ecommerce/domain/message/MessageCommand.java
+++ src/main/java/kr/hhplus/be/ecommerce/domain/message/MessageCommand.java
@@ -2,11 +2,11 @@ package kr.hhplus.be.ecommerce.domain.message;
 
 import kr.hhplus.be.ecommerce.domain.order.OrderEvent;
 import lombok.Builder;
-
-import java.time.LocalDateTime;
+import lombok.Getter;
 
 public class MessageCommand {
 
+    @Getter
     public static class Order {
 
         private final Long orderId;
@@ -14,26 +14,23 @@ public class MessageCommand {
         private final Long userCouponId;
         private final long totalPrice;
         private final long discountPrice;
-        private final LocalDateTime paidAt;
 
         @Builder
-        private Order(Long orderId, Long userId, Long userCouponId, long totalPrice, long discountPrice, LocalDateTime paidAt) {
+        private Order(Long orderId, Long userId, Long userCouponId, long totalPrice, long discountPrice) {
             this.orderId = orderId;
             this.userId = userId;
             this.userCouponId = userCouponId;
             this.totalPrice = totalPrice;
             this.discountPrice = discountPrice;
-            this.paidAt = paidAt;
         }
 
-        public static Order of(OrderEvent.Paid event) {
+        public static Order of(OrderEvent.Completed event) {
             return Order.builder()
                 .orderId(event.getOrderId())
                 .userId(event.getUserId())
                 .userCouponId(event.getUserCouponId())
                 .totalPrice(event.getTotalPrice())
                 .discountPrice(event.getDiscountPrice())
-                .paidAt(event.getPaidAt())
                 .build();
         }
     }
