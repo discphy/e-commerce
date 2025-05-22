3fc512eeb1b41a75d37ae2303493d369291a549d
discphy <dinophy@nate.com>
2025-05-22 22:39:18 +0900

    [FEAT] 비동기 설정 추가

---
+++ src/main/java/kr/hhplus/be/ecommerce/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package kr.hhplus.be.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
