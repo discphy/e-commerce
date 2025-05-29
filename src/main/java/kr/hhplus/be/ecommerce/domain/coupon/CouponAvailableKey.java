54c8e3764b2ce78ff17e3d851bd5985a08fd1b41
discphy <dinophy@nate.com>
2025-05-29 23:37:28 +0900

    [REFACTOR] Kafka를 활용한 쿠폰 발급 프로세스 변경

--- src/main/java/kr/hhplus/be/ecommerce/domain/coupon/CouponAvailableKey.java
+++ src/main/java/kr/hhplus/be/ecommerce/domain/coupon/CouponAvailableKey.java
@@ -5,21 +5,21 @@ import kr.hhplus.be.ecommerce.support.key.KeyType;
 
 import java.util.List;
 
-public class CouponKey implements KeyGeneratable {
+public class CouponAvailableKey implements KeyGeneratable {
 
     private final Long couponId;
 
-    private CouponKey(Long couponId) {
+    private CouponAvailableKey(Long couponId) {
         this.couponId = couponId;
     }
 
-    public static CouponKey of(Long couponId) {
-        return new CouponKey(couponId);
+    public static CouponAvailableKey of(Long couponId) {
+        return new CouponAvailableKey(couponId);
     }
 
     @Override
     public KeyType type() {
-        return KeyType.COUPON;
+        return KeyType.COUPON_AVAILABLE;
     }
 
     @Override
--- src/main/java/kr/hhplus/be/ecommerce/infrastructure/coupon/event/CouponEventPublisherImpl.java
+++ src/main/java/kr/hhplus/be/ecommerce/infrastructure/coupon/event/CouponEventPublisherImpl.java
@@ -2,28 +2,26 @@ package kr.hhplus.be.ecommerce.infrastructure.coupon.event;
 
 import kr.hhplus.be.ecommerce.domain.coupon.CouponEvent;
 import kr.hhplus.be.ecommerce.domain.coupon.CouponEventPublisher;
+import kr.hhplus.be.ecommerce.support.event.EventType;
+import kr.hhplus.be.ecommerce.support.outbox.OutboxEventPublisher;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class CouponSpringEventPublisher implements CouponEventPublisher {
+public class CouponEventPublisherImpl implements CouponEventPublisher {
 
     private final ApplicationEventPublisher eventPublisher;
+    private final OutboxEventPublisher outboxEventPublisher;
 
     @Override
-    public void used(CouponEvent.Used event) {
-        eventPublisher.publishEvent(event);
+    public void publishRequested(CouponEvent.PublishRequested event) {
+        outboxEventPublisher.publishManualEvent(EventType.COUPON_PUBLISH_REQUESTED, event.getCouponId(), event);
     }
 
     @Override
-    public void useFailed(CouponEvent.UseFailed event) {
-        eventPublisher.publishEvent(event);
-    }
-
-    @Override
-    public void canceled(CouponEvent.Canceled event) {
+    public void published(CouponEvent.Published event) {
         eventPublisher.publishEvent(event);
     }
 }
