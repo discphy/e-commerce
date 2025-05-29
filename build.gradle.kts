57a615fa2bff73625cf3a680b0899bc21f61e05e
discphy <dinophy@nate.com>
2025-05-29 22:22:36 +0900

    [FEAT] Kafka 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
 
 	// QueryDSL
 	implementation("com.querydsl:querydsl-jpa:${querydslVersion}:jakarta")
@@ -66,6 +67,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
+	testImplementation("org.testcontainers:kafka:1.19.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	// RestDocs
