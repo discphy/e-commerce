28f3a702e3cbc68232237eebffe38f95bb03a92a
discphy <dinophy@nate.com>
2025-05-29 23:22:21 +0900

    [REFACTOR] 테스트 Support 클래스 리팩토링

--- src/test/java/kr/hhplus/be/ecommerce/test/support/E2EControllerTestSupport.java
+++ src/test/java/kr/hhplus/be/ecommerce/test/support/E2EControllerTestSupport.java
@@ -2,9 +2,6 @@ package kr.hhplus.be.ecommerce.test.support;
 
 import io.restassured.RestAssured;
 import kr.hhplus.be.ecommerce.test.support.database.DatabaseCleaner;
-import kr.hhplus.be.ecommerce.test.support.database.RedisCacheCleaner;
-import kr.hhplus.be.ecommerce.test.support.database.RedisKeyCleaner;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -17,21 +14,9 @@ public abstract class E2EControllerTestSupport extends IntegrationTestSupport {
     @Autowired
     private DatabaseCleaner databaseCleaner;
 
-    @Autowired
-    private RedisCacheCleaner redisCacheCleaner;
-
-    @Autowired
-    private RedisKeyCleaner redisKeyCleaner;
-
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-    }
-
-    @AfterEach
-    void tearDown() {
         databaseCleaner.clean();
-        redisCacheCleaner.clean();
-        redisKeyCleaner.clean();
     }
 }
