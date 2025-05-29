32a504888a0c448ae08ff9981d3ef0333148cd75
discphy <dinophy@nate.com>
2025-05-29 23:50:37 +0900

    [REFACTOR] 결제 시, 포인트 차감 및 쿠폰 사용 로직 추가

--- src/main/java/kr/hhplus/be/ecommerce/domain/payment/PaymentCommand.java
+++ src/main/java/kr/hhplus/be/ecommerce/domain/payment/PaymentCommand.java
@@ -13,17 +13,19 @@ public class PaymentCommand {
 
         private final Long orderId;
         private final Long userId;
+        private final Long userCouponId;
         private final long amount;
 
         @Builder
-        private Payment(Long orderId, Long userId, long amount) {
+        private Payment(Long orderId, Long userId, Long userCouponId, long amount) {
             this.orderId = orderId;
             this.userId = userId;
+            this.userCouponId = userCouponId;
             this.amount = amount;
         }
 
-        public static Payment of(Long orderId, Long userId, long amount) {
-            return new Payment(orderId, userId, amount);
+        public static Payment of(Long orderId, Long userId, Long userCouponId, long amount) {
+            return new Payment(orderId, userId, userCouponId, amount);
         }
     }
 }
