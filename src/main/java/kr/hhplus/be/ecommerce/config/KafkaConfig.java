c1ef732dd6d471d8f0dc52d1bbe0399d8beca510
discphy <dinophy@nate.com>
2025-06-06 00:39:52 +0900

    [REFACTOR] 카프카 에러 로깅 추가 작성

--- src/main/java/kr/hhplus/be/ecommerce/config/KafkaConfig.java
+++ src/main/java/kr/hhplus/be/ecommerce/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package kr.hhplus.be.ecommerce.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,6 +12,7 @@ import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
+@Slf4j
 @Configuration
 public class KafkaConfig {
 
@@ -22,9 +25,28 @@ public class KafkaConfig {
         factory.setConsumerFactory(consumerFactory);
         factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
 
-        DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
-        factory.setCommonErrorHandler(new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3)));
+        factory.setCommonErrorHandler(errorHandler(kafkaTemplate));
 
         return factory;
+    }
+
+    @Bean
+    public DefaultErrorHandler errorHandler(KafkaTemplate<?, ?> template) {
+        var errorHandler = new DefaultErrorHandler(
+            deadLetterPublishingRecoverer(template),
+            new FixedBackOff(1000L, 3)
+        );
+
+        errorHandler.setLogLevel(KafkaException.Level.ERROR);
+
+        return errorHandler;
+    }
+
+    @Bean
+    public DeadLetterPublishingRecoverer deadLetterPublishingRecoverer(KafkaTemplate<?, ?> template) {
+        return new DeadLetterPublishingRecoverer(template, (r, e) -> {
+            log.error("카프카 핸들러 예외 : {}", e.getMessage(), e);
+            return null;
+        });
     }
 }
